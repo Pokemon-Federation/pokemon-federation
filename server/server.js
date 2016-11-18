9b14c74aff097bc23522cf5b0ab473b0692e188e
shawn-southwell <shawn.southwel@gmail.com>
2016-11-17 22:19:24 -0800

    Fetch and format data from first 151, still need to add to DB

--- server/server.js
+++ server/server.js
@@ -4,29 +4,57 @@ const app = express();
 const Pokemon = require('./models/pokemon_model');
 const User = require('./models/user_model');
 const Team = require('./models/team_model');
-const bluebird = require('bluebird');
+const Promise = require('bluebird');
 const request = require('request-promise');
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.json())
 app.listen(3000, () => console.log('servering port 3000'));
-var pokemonNum = 1;
-const promises = [];
-const url = 'http://pokeapi.co/api/v1/pokemon/'
-while (pokemonNum <= 151) {
-  var num = pokemonNum;
-  promises.push(new Promise((resolve, reject) => {
-    request(url + num.toString(), (error, response) => {
-      console.log('response',num,JSON.parse(response.body).name);
-      const pokeObj = {};
-       pokeObj.name = JSON.parse(response.body).name;
-      if (!error) {
-        resolve(pokeObj);
-      }
-    })
-  }))
-  pokemonNum++;
+
+const url = 'http://pokeapi.co/api/v1/pokemon/';
+
+function createReqObject(num){
+  const requests = [];
+  var index = 1;
+  while(index <= num){
+    requests.push({ url: `${url+index++}`});
+  }
+  return requests;
 }
+
+function getTypes(arr){
+  if (arr.length === 1) return arr[0].name;
+  return arr.reduce((types,item) => {
+    types.push(item.name);
+    return types;
+  },[]);
+}
+const requests = createReqObject(151);
+
+Promise.map(requests,function(obj) {
+  return request(obj).then(function(body) {
+    return JSON.parse(body);
+  });
+}).then(function(results) {
+  const data = [];
+  results.forEach((pokemon) => {
+    const pokeObj = {};
+    pokeObj.name = pokemon.name;
+    pokeObj.type = getTypes(pokemon.types);
+    pokeObj.hp = pokemon.hp;
+    pokeObj.attack = pokemon.attack;
+    pokeObj.defense = pokemon.defense;
+    pokeObj.sp_atk = pokemon.sp_atk;
+    pokeObj.sp_def = pokemon.sp_def;
+    pokeObj.speed = pokemon.speed;
+    data.push(pokeObj);
+  })
+  //write to the DB with this pokedata braj :)
+  console.log(data);
+}, function(err) {
+  console.log(err)
+});
+
 
 
 
